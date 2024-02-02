512922e1493ba1e8fc38cc73bdc9e24f0f1e71c3
agusbw <agusbudhiarta81@gmail.com>
2024-02-02 14:09:29 +0800

    Update Warga Dashboard Page

--- app/(user)/warga/(dashboard)/loading.tsx
+++ app/(user)/warga/(dashboard)/loading.tsx
@@ -6,6 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
   Mails,
   MailX,
@@ -15,91 +16,124 @@ import {
 } from "lucide-react";
 import { SuratCountCardSkeleton } from "@/components/dashboard/surat-count-card";
 import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
 
 export default async function Loading() {
   return (
-    <DashboardContainer title="Dashboard">
-      <>
-        <div className="flex-col md:flex">
-          <div className="flex-1 space-y-4">
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-5">
-              <SuratCountCardSkeleton
-                title="Total Surat"
-                className="bg-primary"
-                Icon={Mails}
-              />
-              <SuratCountCardSkeleton
-                title="Surat Pending"
-                className="bg-yellow-500"
-                Icon={MailQuestion}
-              />
-              <SuratCountCardSkeleton
-                className="bg-green-500"
-                title="Surat Selesai"
-                Icon={MailCheck}
-              />
-              <SuratCountCardSkeleton
-                className="bg-blue-500"
-                title="Surat Diambil"
-                Icon={CheckCheck}
-              />
-              <SuratCountCardSkeleton
-                className="bg-red-500"
-                title="Surat Ditolak"
-                Icon={MailX}
-              />
-            </div>
-            <div className="grid lg:gap-4 gap-y-4 lg:gap-y-0 grid-cols-2 lg:grid-cols-7">
-              <Card className="col-span-2">
-                <CardHeader>
-                  <CardTitle>Pengajuan Surat Terakhir</CardTitle>
-                  <CardDescription>
-                    4 data pengajuan surat terakhir yang anda lakukan
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <div className="space-y-6">
-                    {[1, 2, 3, 4].map((i) => (
-                      <div
-                        className="flex items-center"
-                        key={i}
-                      >
-                        <div className="space-y-1">
-                          <p className="text-sm font-medium leading-none">
-                            <Skeleton className="w-36 h-4" />
-                          </p>
-                          <p className="text-sm text-muted-foreground">
-                            <Skeleton className="w-24 h-4" />
-                          </p>
-                        </div>
-                        <div className="ml-auto font-medium">
-                          <Skeleton className="w-16 h-4" />
-                        </div>
+    <DashboardContainer title="Dashboard Warga">
+      <div className="mb-4">
+        <p className="text-2xl font-medium text-primary">Selamat Datang, </p>
+        <p className="text-lg text-muted-foreground">
+          Untuk mengajukan surat keterangan, silakan ke menu{" "}
+          <Link
+            href={"/warga/pengajuan"}
+            className="text-primary hover:underline"
+          >
+            Ajukan Surat
+          </Link>
+        </p>
+      </div>
+      <div className="flex-col md:flex">
+        <div className="flex-1 space-y-4">
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-5">
+            <SuratCountCardSkeleton
+              title="Total Surat"
+              className="bg-primary"
+              Icon={Mails}
+            />
+            <SuratCountCardSkeleton
+              title="Surat Pending"
+              className="bg-yellow-500"
+              Icon={MailQuestion}
+            />
+            <SuratCountCardSkeleton
+              className="bg-green-500"
+              title="Surat Selesai"
+              Icon={MailCheck}
+            />
+            <SuratCountCardSkeleton
+              className="bg-blue-500"
+              title="Surat Diambil"
+              Icon={CheckCheck}
+            />
+            <SuratCountCardSkeleton
+              className="bg-red-500"
+              title="Surat Ditolak"
+              Icon={MailX}
+            />
+          </div>
+          <div className="grid lg:gap-4 gap-y-4 lg:gap-y-0 grid-cols-2 lg:grid-cols-7">
+            <Card className="col-span-2">
+              <CardHeader>
+                <CardTitle>Pengajuan Surat Terakhir</CardTitle>
+                <CardDescription>
+                  Data pengajuan surat terakhir yang anda lakukan.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="space-y-6">
+                  {[1, 2, 3, 4].map((i) => (
+                    <div
+                      className="flex items-center"
+                      key={i}
+                    >
+                      <div className="space-y-1">
+                        <p className="text-sm font-medium leading-none">
+                          <Skeleton className="w-36 h-4" />
+                        </p>
+                        <p className="text-sm text-muted-foreground">
+                          <Skeleton className="w-24 h-4" />
+                        </p>
                       </div>
-                    ))}
-                  </div>
-                </CardContent>
-              </Card>
-              <Card className="col-span-3">
-                <CardHeader>
-                  <CardTitle>Grafik Pengajuan Surat</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <Skeleton className="w-full h-64" />
-                </CardContent>
-              </Card>
-              <Card className="col-span-2">
-                <CardHeader>
-                  <CardTitle>Grafik Jenis Surat</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <Skeleton className="w-full h-64" />
-                </CardContent>
-              </Card>
-            </div>
+                      <div className="ml-auto font-medium">
+                        <Skeleton className="w-16 h-4" />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </CardContent>
+            </Card>
+            <Card className="col-span-5">
+              <CardHeader className="text-xl">
+                <CardTitle>Arti Status Surat</CardTitle>
+                <CardDescription>
+                  Penjelasan mengenai status surat yang anda ajukan.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-y-2">
+                <Alert className="bg-yellow-500/10 text-yellow-600">
+                  <AlertTitle>Pending</AlertTitle>
+                  <AlertDescription>
+                    Status pending menandakan bahwa surat anda sedang dalam
+                    proses verifikasi oleh admin
+                  </AlertDescription>
+                </Alert>
+                <Alert className="bg-green-500/10 text-green-600">
+                  <AlertTitle>Selesai</AlertTitle>
+                  <AlertDescription>
+                    Status selesai menandakan bahwa surat anda telah selesai
+                    diproses dan siap diambil di kantor desa.
+                  </AlertDescription>
+                </Alert>
+                <Alert className="bg-blue-500/10 text-blue-600">
+                  <AlertTitle>Diambil</AlertTitle>
+                  <AlertDescription>
+                    Status diambil menandakan bahwa anda telah mengambil surat
+                    tersebut di kantor desa.
+                  </AlertDescription>
+                </Alert>
+                <Alert className="bg-red-500/10 text-red-600">
+                  <AlertTitle>Ditolak</AlertTitle>
+                  <AlertDescription>
+                    Status ditolak menandakan bahwa surat anda ditolak oleh
+                    admin dengan alasan tertentu.
+                  </AlertDescription>
+                </Alert>
+              </CardContent>
+            </Card>
           </div>
         </div>
-      </>
+      </div>
     </DashboardContainer>
   );
 }
--- app/(user)/warga/(dashboard)/page.tsx
+++ app/(user)/warga/(dashboard)/page.tsx
@@ -8,8 +8,9 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import SuratCountCard from "@/components/dashboard/surat-count-card";
-import { BarChartSurat, PieChartSurat } from "@/components/dashboard/charts";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { UserLatestRequest as LatestRequest } from "@/components/dashboard/latest-request";
+import Link from "next/link";
 import {
   Mails,
   MailX,
@@ -17,12 +18,7 @@ import {
   MailQuestion,
   CheckCheck,
 } from "lucide-react";
-import {
-  fetchUserTotalSurat,
-  fetchUserTotalSuratByKategori,
-  fetchUserBarChartData,
-  fetchUserLatestSurat,
-} from "@/lib/data";
+import { fetchUserTotalSurat, fetchUserLatestSurat } from "@/lib/data";
 
 export const metadata: Metadata = {
   title: "Overview",
@@ -34,21 +30,30 @@ export const revalidate = 0;
 
 export default async function WargaDashboardPage() {
   const suratPromise = fetchUserTotalSurat();
-  const suratByKategoriPromise = fetchUserTotalSuratByKategori();
-  const suratByMonthPromise = fetchUserBarChartData();
   const userLatestSuratPromise = fetchUserLatestSurat();
 
-  const [surat, suratByKategori, suratByMonth, userLatestSurat] =
-    await Promise.all([
-      suratPromise,
-      suratByKategoriPromise,
-      suratByMonthPromise,
-      userLatestSuratPromise,
-    ]);
+  const [surat, userLatestSurat] = await Promise.all([
+    suratPromise,
+    userLatestSuratPromise,
+  ]);
+
+  await new Promise((resolve) => setTimeout(resolve, 4000));
 
   return (
-    <DashboardContainer title="Dashboard">
+    <DashboardContainer title="Dashboard Warga">
       <>
+        <div className="mb-4">
+          <p className="text-2xl font-medium text-primary">Selamat Datang, </p>
+          <p className="text-lg text-muted-foreground">
+            Untuk mengajukan surat keterangan, silakan ke menu{" "}
+            <Link
+              href={"/warga/pengajuan"}
+              className="text-primary hover:underline"
+            >
+              Ajukan Surat
+            </Link>
+          </p>
+        </div>
         <div className="flex-col md:flex">
           <div className="flex-1 space-y-4">
             <div className="grid gap-4 grid-cols-2 lg:grid-cols-5">
@@ -88,27 +93,49 @@ export default async function WargaDashboardPage() {
                 <CardHeader>
                   <CardTitle>Pengajuan Surat Terakhir</CardTitle>
                   <CardDescription>
-                    4 data pengajuan surat terakhir yang anda lakukan
+                    Data pengajuan surat terakhir yang anda lakukan.
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
                   <LatestRequest data={userLatestSurat} />
                 </CardContent>
               </Card>
-              <Card className="col-span-3">
-                <CardHeader>
-                  <CardTitle>Grafik Pengajuan Surat</CardTitle>
+              <Card className="col-span-5">
+                <CardHeader className="text-xl">
+                  <CardTitle>Arti Status Surat</CardTitle>
+                  <CardDescription>
+                    Penjelasan mengenai status surat yang anda ajukan.
+                  </CardDescription>
                 </CardHeader>
-                <CardContent className="-ml-10">
-                  <BarChartSurat data={suratByMonth} />
-                </CardContent>
-              </Card>
-              <Card className="col-span-2">
-                <CardHeader>
-                  <CardTitle>Grafik Jenis Surat</CardTitle>
-                </CardHeader>
-                <CardContent className="pl-2">
-                  <PieChartSurat data={suratByKategori} />
+                <CardContent className="flex flex-col gap-y-2">
+                  <Alert className="bg-yellow-500/10 text-yellow-600">
+                    <AlertTitle>Pending</AlertTitle>
+                    <AlertDescription>
+                      Status pending menandakan bahwa surat anda sedang dalam
+                      proses verifikasi oleh admin
+                    </AlertDescription>
+                  </Alert>
+                  <Alert className="bg-green-500/10 text-green-600">
+                    <AlertTitle>Selesai</AlertTitle>
+                    <AlertDescription>
+                      Status selesai menandakan bahwa surat anda telah selesai
+                      diproses dan siap diambil di kantor desa.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert className="bg-blue-500/10 text-blue-600">
+                    <AlertTitle>Diambil</AlertTitle>
+                    <AlertDescription>
+                      Status diambil menandakan bahwa anda telah mengambil surat
+                      tersebut di kantor desa.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert className="bg-red-500/10 text-red-600">
+                    <AlertTitle>Ditolak</AlertTitle>
+                    <AlertDescription>
+                      Status ditolak menandakan bahwa surat anda ditolak oleh
+                      admin dengan alasan tertentu.
+                    </AlertDescription>
+                  </Alert>
                 </CardContent>
               </Card>
             </div>
