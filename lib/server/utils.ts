9363c7c77e109ee48c4a8cf6fa8caa17aed61736
agusbw <agusbudhiarta81@gmail.com>
2024-03-27 17:25:22 +0800

    Fix encryption algorithm in utils.ts

--- lib/server/utils.ts
+++ lib/server/utils.ts
@@ -1,36 +1,26 @@
 import crypto from "crypto";
 
 export function encryptData(data: string, key: string) {
-  const iv = Buffer.alloc(16, 0);
   const cipher = crypto.createCipheriv(
-    "aes-256-cbc",
+    "aes-256-ecb",
     Buffer.from(key, "hex"),
-    iv
+    null
   );
   let encryptedData = cipher.update(data, "utf-8", "hex");
   encryptedData += cipher.final("hex");
-  return iv.toString("hex") + ":" + encryptedData;
+  return encryptedData;
 }
 
 export function decryptData(encryptedData: string, key: string) {
   if (!key) {
     throw new Error("Decryption key is required.");
   }
-  const parts = encryptedData.split(":");
-  if (parts.length !== 2) {
-    throw new Error("Invalid encrypted data format.");
-  }
-  const [ivString, encryptedString] = parts;
-  if (!ivString || !encryptedString) {
-    throw new Error("Initialization vector or encrypted data is missing.");
-  }
-  const iv = Buffer.from(ivString, "hex");
   const decipher = crypto.createDecipheriv(
-    "aes-256-cbc",
+    "aes-256-ecb",
     Buffer.from(key, "hex"),
-    iv
+    null
   );
-  let decryptedData = decipher.update(Buffer.from(encryptedString, "hex"));
+  let decryptedData = decipher.update(Buffer.from(encryptedData, "hex"));
   decryptedData = Buffer.concat([decryptedData, decipher.final()]);
   return decryptedData.toString("utf-8");
 }
